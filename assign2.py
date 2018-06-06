b42a59b46573bea0f1297f04da35d80d3d6b6203
sonalinegi <sonali3098@gmail.com>
2018-06-06 14:09:43 +0700

    pyhton file uploaded to git

---
+++ assign2.py
@@ -0,0 +1,17 @@
+#question no 1
+print("hey beautiful have a nice day")
+#question no 2
+print("acad" + "view")
+#quesion no 3
+x=input("enter x:")
+print(x)
+y=input("enter y:")
+print(y)
+z=input("enter z:")
+print(z)
+#question no 4
+print("lets get started")
+#quetion no  6
+name="tony stark\t"
+salary=1000000
+print('%s''%d'%(name,salary))
