a7987220e0e87cbb5a2b9f5c25f8d6721573de76
sonalinegi <sonali3098@gmail.com>
2018-06-29 10:29:18 +0700

    gui-2

---
+++ gui2.py
@@ -0,0 +1,39 @@
+#Create a dictionary with name and mobile number. Define a GUI interface using tkinter and pack the label and create a scrollbar to scroll the list of keys in the dictionary.
+import tkinter
+from tkinter import *
+tk=Tk()
+tk.title("GUI")
+data={'sonali':'9760993285','antra':'72184974790','simran':'78985875768','himadri':'9126675432'}
+Label(tk,text="Enter your name").pack()
+e1=Entry(tk)
+e1.pack()
+scrollbar=Scrollbar(tk)
+scrollbar.pack(side=RIGHT,fill=Y)
+mylist=Listbox(tk,yscrollcommand=scrollbar.set)
+for key in data.keys():
+    mylist.insert(END,key)
+mylist.pack(side=LEFT)
+scrollbar.config(command=mylist.yview)
+Button(tk,text="Exit",fg="red",bg="white",command=tk.destroy).pack()
+tk.mainloop()
+
+#In the same tkinter file as above,create a function to insert items into the dictionary.
+import tkinter
+from tkinter import *
+tk=tkinter.Tk()
+tk.title("details")
+label1=Label(tk,text="Enter your name").grid(row=0)
+label2=Label(tk,text="Enter your phone number").grid(row=1)
+x=StringVar()
+y=IntVar()
+e1=Entry(tk,textvariable=x)
+e2=Entry(tk,textvariable=y)
+a={'sonali':9760993285,'antra':72184974790,'simran':78985875768,'himadri':9126675432}
+def insert_items(x,y):
+    a.update({x:int(y)})
+    print(a)
+e1.grid(row=0,column=1)
+e2.grid(row=1,column=1)
+Button(tk,text="Execute",fg="green",bg="white",command=lambda :insert_items(x.get(),y.get())).grid(row=3,column=0,sticky=W,pady=4)
+Button(tk,text="Exit",fg="red",bg="white",command=tk.destroy).grid(row=3,column=1,sticky=W,pady=4)
+tk.mainloop()
